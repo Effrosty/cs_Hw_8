41a44a66b113d987a0a3a7f615d6e97d36c36882
Egor <effrosty@ya.ru>
2022-09-05 19:28:53 +0300

    1

--- one/Program.cs
+++ one/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("введите количество строк");
+﻿//54
+
+
+Console.WriteLine("введите количество строк");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов");
 int columnsVol = Convert.ToInt32(Console.ReadLine());
